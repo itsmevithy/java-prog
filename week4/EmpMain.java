5eee67c78838c48d3bad6526658aab392bb09660
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-08-24 09:35:47 +0530

    Add files via upload

--- week4/EmpMain.java
+++ week4/EmpMain.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 class Employee{
-	String name, id, department, designation;
-	double salary;
+	protected String name, id, department, designation;
+	protected double salary;
 	Employee(){}
 	Employee(String name1, String id1, String department1, String designation1, double salary1){
 		name=name1;
@@ -18,22 +18,54 @@ class Employee{
 		designation=p.designation;
 		salary=p.salary;
 	}
-	void displayDetails(){
-		System.out.println("Name: "+name+"\nID: "+id+"\nDept: "+department+"\nDesignation: "+designation+"\nSalary: "+salary);
+    public String toString(){
+        return String.valueOf("Name: "+name+"\nID: "+id+"\nDept: "+department+"\nDesignation: "+designation+"\nSalary: "+salary);
+    }
+	public void displayDetails(){
+		System.out.println(this);
 	}
 }
 
 class Manager extends Employee{
 	double bonus;
-	double computeSalary(double bonusamount){
+	public double computeSalary(double bonusamount){
 		return salary+bonusamount;
 	}
-	void displayNew(){
-		this.displayDetails();
-		System.out.println("Bonus: "+bonus);
+    public String toString(){
+        return String.valueOf(this.toString()+"\nBonus: "+bonus);
+    }
+	public void displayNew(){
+		System.out.println(this);
 	}
 	Manager(String name1, String id1, String department1, String designation1, double salary1, double bonus1){
 		super(name1, id1, department1, designation1, salary1);
+        salary=this.computeSalary(bonus1);
 		bonus=bonus1;
-	}
+	} 
+}
+
+class EmpMain{
+    public static void main(String a[]){
+        Scanner scn=new Scanner(System.in);
+        Manager m[]=new Manager[2];
+        for(int i=0; i<2; i++){
+            System.out.println("Enter name: ");
+            String name=scn.nextLine();
+            System.out.println("Enter ID: ");
+            String id=scn.nextLine();
+            System.out.println("Enter dept: ");
+            String dept=scn.nextLine();
+            System.out.println("Enter designation: ");
+            String desig=scn.nextLine();
+            System.out.println("Enter salary: ");
+            double salary=scn.nextDouble();
+            System.out.println("Enter bonus: ");
+            double bonus=scn.nextDouble();
+            scn.skip("[\r\n+]");
+            scn.skip("[\r\n+]");
+            m[i]= new Manager(name, id, dept, desig, salary, bonus);
+        }
+        for(int i=0; i<2; i++) m[i].displayNew();
+        scn.close();
+    }
 }
