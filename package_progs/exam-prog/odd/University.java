8e271bc6a11a292c6bc45a560836dbcdfa94fdb6
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-10-08 20:36:47 +0530

    Delete package_progs/exam-prog/odd/University.java

--- package_progs/exam-prog/odd/University.java
+++
@@ -1,8 +0,0 @@
-package Uni;
-
-public abstract class University{
-	public static String uni_name;
-	public String[] courses={"Not Alloted", "BBA", "MCA", "MBA"};
-	public static int max[]=new int[3];
-	public abstract void displayDetails();
-}
