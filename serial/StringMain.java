4ed3639f6a4e56ca845a45fcf2e2c2e318ea666a
itsmevithy <lordvithu24@gmail.com>
2023-10-13 10:24:39 +0530

    StringMain update

--- serial/StringMain.java
+++ serial/StringMain.java
@@ -1,27 +1,50 @@
 import java.util.*;
 
 class Stringops{
-	void reverse(ArrayList<String> words){
+	private ArrayList<String> words;
+	private int n;
+	Stringops(){
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter number of words to be entered: ");
+		n=in.nextInt();
+		words=new ArrayList<String>();
+		for(int i=0; i<n; i++) words.add(in.next());
+	}
+	void reverse(){
 		ArrayList<String> temp=new ArrayList<String>();
-		Iterator w=words.iterator();
-		while(w.hasNext()) temp.add((String)w.next());
-		Iterator t=temp.iterator();
+		for(int i=n-1; i>=0; i--) temp.add(words.get(i));
 		words.removeAll(temp);
-		for(int i=words.size()-1; i>0; i--) words.add(i, (String)t.next());
-		w=words.iterator();
-		while(w.hasNext()) System.out.println(w.next());
-		System.out.println("Done");
+		for(int i=0; i<n; i++) words.add(temp.get(i));
+	}
+	void capitalizePlurals(){
+		for(int i=0; i<n; i++) if(((String)words.get(i)).endsWith("s")) words.set(i, ((String)words.get(i)).toUpperCase());
+	}
+	void removePlurals(){
+		for(int i=0; i<words.size(); i++) if(((String)words.get(i)).endsWith("s")||((String)words.get(i)).endsWith("S")) words.remove(i);
+		n=words.size();
+	}
+	void display(){	
+		for(int i=0; i<n; i++) System.out.print(words.get(i)+((i!=n-1)?", ":"\n"));
 	}
 }
 
 class StringMain{
 	public static void main(String args[]){
-			Stringops a=new Stringops();
-			Scanner in=new Scanner(System.in);
-			ArrayList<String> list=new ArrayList<String>();
-			for(int i=0; i<5; i++) list.add(in.next());
-			Iterator bef=list.iterator();
-			a.reverse(list);
-			while(bef.hasNext()) System.out.println("la "+bef.next());
+			int i;
+			Stringops a= new Stringops();
+			System.out.println("\nBefore changes: ");
+			a.display();
+
+			a.reverse();
+			System.out.println("\nAfter reverse: ");
+			a.display();
+			
+			a.capitalizePlurals();
+			System.out.println("\nAfter capitalising plurals: ");
+			a.display();
+			
+			a.removePlurals();	
+			System.out.println("\nAfter removing plurals: ");
+			a.display();
 	}
 }
